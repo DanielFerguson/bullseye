ae43debec30aca380013ca9f80ec3e4f9ebd0382
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-13 14:12:29 +1100

    added prisma fetching to archers api src

--- api/src/archers.ts
+++ api/src/archers.ts
@@ -1,11 +1,21 @@
-// TODO
+import { PrismaClient } from '@prisma/client';
+
+interface ApiEvent {
+  pathParameters: {
+    archerId: number;
+  }
+}
+
+const prisma = new PrismaClient();
+
 module.exports.getAll = async (event) => {
+  const archers = await prisma.user.findMany();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        archers: archers
       },
       null,
       2
@@ -29,13 +39,14 @@ module.exports.create = async (event) => {
 };
 
 // TODO
-module.exports.get = async (event) => {
+module.exports.get = async (event: ApiEvent) => {
+  const id = event.pathParameters.archerId;
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        message: `Getting Archer #${id}`,
       },
       null,
       2
