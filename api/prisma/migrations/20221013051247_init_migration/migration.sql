14f1deab5a7c7b2fbc1fc64efef9d5cb88810a1d
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-13 20:41:17 +1100

    🗃️ updated database migrations

--- api/prisma/migrations/20221013051247_init_migration/migration.sql
+++ api/prisma/migrations/20221013051247_init_migration/migration.sql
@@ -3,7 +3,7 @@ CREATE TYPE "GameType" AS ENUM ('ABA');
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
@@ -15,21 +15,8 @@ CREATE TABLE "User" (
 CREATE TABLE "Game" (
     "id" TEXT NOT NULL,
     "type" "GameType" NOT NULL DEFAULT 'ABA',
-    "clubId" TEXT NOT NULL,
 
     CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Club" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "streetAddress" TEXT,
-    "city" TEXT NOT NULL,
-    "logoUrl" TEXT,
-    "url" TEXT,
-
-    CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -38,12 +25,13 @@ CREATE TABLE "Round" (
     "roundNumber" INTEGER NOT NULL,
     "score" INTEGER NOT NULL,
     "gameId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Round_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Game" ADD CONSTRAINT "Game_clubId_fkey" FOREIGN KEY ("clubId") REFERENCES "Club"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Round" ADD CONSTRAINT "Round_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Round" ADD CONSTRAINT "Round_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Round" ADD CONSTRAINT "Round_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
