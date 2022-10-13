8a587c27a9bba756f73564f7dec24545d79932fc
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-13 20:40:52 +1100

    ✨ added helper functions

---
+++ api/src/helpers.ts
@@ -0,0 +1,4 @@
+export function stringIsUuid(str: string): boolean {
+    const uuidExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+    return uuidExp.test(str);
+}
